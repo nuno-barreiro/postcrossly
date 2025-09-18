a8924f8d3dc36c8bf86af00016d1afc730cdb78d
Nuno Barreiro <nuno.barreiro@outlook.com>
2025-09-18 22:42:59 +0100

    Refactor main.dart and postcard.dart for improved readability and documentation; enhance postcards_repository.dart with additional comments and methods

--- lib/models/postcard.dart
+++ lib/models/postcard.dart
@@ -1,7 +1,10 @@
 import 'package:uuid/uuid.dart';
 
+/// Model class for a postcard
 class Postcard {
+  /// Unique identifier for the postcard
   final String id;
 
+  /// Constructor
   Postcard() : id = const Uuid().v4();
 }
--- lib/repositories/postcards_repository.dart
+++ lib/repositories/postcards_repository.dart
@@ -1,20 +1,44 @@
 import 'package:postcrossly/models/postcard.dart';
 
+/// Repository class for managing postcards
 class PostcardsRepository {
+  /// In-memory list to store postcards
   final List<Postcard> _postcards = [];
 
+  /// Constructor
+  PostcardsRepository();
+
+  /// Adds a postcard to the repository
+  ///
+  /// Parameters:
+  /// [postcard] The postcard to add
+  ///
+  /// Returns:
+  /// The added postcard
+  ///
+  /// Throws:
+  /// Exception if the postcard could not be added
   Postcard addPostcard(Postcard postcard) {
     _postcards.add(postcard);
     return postcard;
   }
 
-  List<Postcard> fetchPostcards() {
-    return _postcards;
-  }
+  /// Fetches all postcards from the repository
+  ///
+  /// Returns:
+  /// A list of all postcards in the repository
+  List<Postcard> fetchPostcards() => _postcards;
 
+  /// Retrieves a postcard by its ID
+  ///
+  /// Parameters:
+  /// [id] The ID of the postcard to retrieve
+  ///
+  /// Returns:
+  /// The postcard with the specified ID, or null if not found
   Postcard? getPostcard(String id) {
     try {
-      return _postcards.singleWhere((postcard) => postcard.id == id);
+      return _postcards.singleWhere((p) => p.id == id);
     } catch (e) {
       return null;
     }
